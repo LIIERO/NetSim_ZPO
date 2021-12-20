48f73a7d477c00c17cb37d73be838686f6c08dbc
LIIERO <82181471+LIIERO@users.noreply.github.com>
2021-12-20 16:34:28 +0100

    Add files via upload

--- src/package.cpp
+++ src/package.cpp
@@ -2,17 +2,35 @@
 #include "types.hpp"
 #include "package.hpp"
 
+std::set<ElementID> Package::assigned_IDs;
+std::set<ElementID> Package::freed_IDs;
+
+Package::Package() {
+    ElementID new_id;
+    if (!freed_IDs.empty()) {
+        auto it = freed_IDs.begin();
+        new_id = *it;
+        freed_IDs.erase(it);
+
+    } else {
+        if (assigned_IDs.empty()) {
+            new_id = 1;
+        } else {
+            new_id = *(assigned_IDs.rbegin());
+            ++new_id;
+        }
+    }
+
+    _id = new_id;
+    assigned_IDs.insert(new_id);
+}
 
 Package::~Package() {
-
+    assigned_IDs.erase(_id);
+    freed_IDs.insert(_id);
 }
 
 Package& Package::operator=(Package&& package) {
-    id_ = package.id_;
-
+    _id = package._id;
     return (*this);
-}
-
-Package::Package(Package&& package) {
-
 }
