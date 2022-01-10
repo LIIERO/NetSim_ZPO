de7cf3c6264853f5a68dd099df8b0d8f5893496a
Piotr Mamos <pioacb@gmail.com>
2022-01-10 20:14:24 +0100

    Fabryka2

--- include/factory.hpp
+++ include/factory.hpp
@@ -3,25 +3,96 @@
 #define NETSIM_ZPO_FACTORY1_HPP
 
 #include "types.hpp"
-#include "NodeCollection.hpp"
-#include "Ramps.hpp"
-#include "Workers.hpp"
-#include "Storehouses.hpp"
+#include "nodes.hpp"
+
+template <class Node> class NodeCollection{
+
+public:
+    using container_t = typename std::vector<Node>;
+    using iterator = typename container_t::iterator;
+    using const_iterator = typename container_t::const_iterator;
+
+    void add(Node&& node){
+        nodes.push_back(node);
+    }
+    //TODO Możliwe że poniżej jest źle
+    void remove_by_id(ElementID id){
+        nodes.erase(std::remove_if(nodes.begin(),nodes.end(), [id](const auto &node){return (node.get_id() == id);}));
+    }
+    NodeCollection<Node>::iterator find_by_id(ElementID id){
+        return std::find_if(nodes.begin(),nodes.end(), [id](const auto &node){return (node.get_id() == id);});
+    };
+    NodeCollection<Node>::const_iterator find_by_id (ElementID id) const{
+        return std::find_if(nodes.cbegin(),nodes.cend(), [id](const auto &node){return (node.get_id() == id);});
+    };
+    /*const_iterator  cbegin(){
+        return nodes.front();
+    };
+    const_iterator  cend(){
+        return nodes.back();
+    };*/
+    iterator begin() { return nodes.begin(); }
+    iterator end() { return nodes.end(); }
+    const_iterator begin() const { return nodes.cbegin(); }
+    const_iterator end() const { return nodes.cend(); }
+    const_iterator cbegin() const { return nodes.cbegin(); }
+    const_iterator cend() const { return nodes.cend(); }
+
+
+protected:
+    container_t nodes;
+};
+
+class Ramps: public NodeCollection<Ramp>{
+public:
+    Ramps() = default;
+
+    ~Ramps() = default;
+};
+
+class Storehouses: public NodeCollection<Storehouse>{
+public:
+    Storehouses() = default;
+
+    ~Storehouses() = default;
+};
+
+class Workers: public NodeCollection<Worker>{
+public:
+    Workers() =default ;
+
+    ~Workers() = default;
+};
 
 class Factory {
 private:
-    Ramps* ramps = new Ramps();
-    Workers* workers = new Workers();
-    Storehouses* storehouses = new Storehouses();
-    void remove_receiver(NodeCollection<IPackageReceiver>& collection, ElementID id);
+    
+    Ramps ramps;
+    Workers workers;
+    Storehouses storehouses;
+
+    template<class Node> void remove_receiver(NodeCollection<Node>& collection, ElementID id){
+        for(auto& node : collection){
+            IPackageReceiver* receiver = nullptr;
+            for (const auto& preferences : node.receiver_preferences_.get_preferences()){
+                if(preferences.first->get_id() == id){
+                    receiver =preferences.first;
+                    break;
+                }
+
+            }
+            if(receiver != nullptr)
+                ((ReceiverPreferences&) node.receiver_preferences_).remove_receiver(dynamic_cast<IPackageReceiver*>(receiver));
+        }
+    }
 
 public:
 
     Factory() = default;
 
-    void add_ramp(Ramp&&);
-    void add_worker(Worker&&);
-    void add_storehouse(Storehouse&&);
+    void add_ramp(Ramp&& ramp);
+    void add_worker(Worker&& worker);
+    void add_storehouse(Storehouse&& storehouse);
 
     void remove_ramp(ElementID id);
     void remove_worker(ElementID id);
@@ -31,9 +102,9 @@ public:
     NodeCollection<Worker>::iterator find_worker_by_id(ElementID id);
     NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id);
 
-    NodeCollection<Ramp>::const_iterator find_ramp_by_cid(ElementID id);
-    NodeCollection<Worker>::const_iterator find_worker_by_cid(ElementID id);
-    NodeCollection<Storehouse>::const_iterator find_storehouse_by_cid(ElementID id);
+    NodeCollection<Ramp>::const_iterator find_ramp_by_id (ElementID id) const;
+    NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id)  const;
+    NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id)  const;
 
     NodeCollection<Ramp>::const_iterator  ramp_cbegin();
     NodeCollection<Worker>::const_iterator  worker_cbegin();
@@ -47,9 +118,13 @@ public:
     void do_deliveries(Time);
     void do_package_passing(void);
     void do_work(Time);
-    ~Factory();
+    ~Factory() = default;
 
 };
 
 enum class NodeColor { UNVISITED, VISITED, VERIFIED };
+
+
+
+
 #endif //NETSIM_ZPO_FACTORY1_HPP
