f4b6141ddfe24372ab9d7743feefa0dc6e20285a
Bartłomiej Matuszewski <bartematuszewski2001@gmail.com>
2022-01-16 19:14:23 +0100

    simulate() finished
    we should use tests

---
+++ src/simulation.cpp
@@ -0,0 +1,15 @@
+#include <stdexcept>
+#include "simulation.hpp"
+void simulate(Factory& f, TimeOffset d, std::function<void (Factory&, Time)> rf){
+    if ( !f.is_consistent() ) {
+        throw std::logic_error("incosistent factory");
+    }
+
+    for (Time t=1; t<=d;t++){
+        f.do_deliveries(t);
+        f.do_package_passing();
+        f.do_work(t);
+        rf(f, t);
+    }
+    return;
+}
